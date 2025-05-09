7f8931986688b33b49423a31d25faaff9360e51e
chaechae <juold0568@naver.com>
2025-05-10 00:59:16 +0900

    feat: 공연 상세 조회 관련 dto, service 추가

---
+++ src/main/java/com/fifo/ticketing/domain/performance/dto/PerformanceMapper.java
@@ -0,0 +1,32 @@
+package com.fifo.ticketing.domain.performance.dto;
+
+import com.fifo.ticketing.domain.performance.entity.Grade;
+import com.fifo.ticketing.domain.performance.entity.Performance;
+
+import java.util.List;
+
+public class PerformanceMapper {
+    public static PerformanceDetailResponse toDetailResponseDto(Performance performance, List<PerformanceSeatGradeDto> seatGrades) {
+        return PerformanceDetailResponse.builder()
+                .performanceId(performance.getId())
+                .title(performance.getTitle())
+                .description(performance.getDescription())
+                .category(performance.getCategory().name())
+                .startTime(performance.getStartTime())
+                .endTime(performance.getEndTime())
+                .placeName(performance.getPlace().getName())
+                .address(performance.getPlace().getAddress())
+                .totalSeats(performance.getPlace().getTotalSeats())
+                .seatGrades(seatGrades)
+                .build();
+
+    }
+
+    public static PerformanceSeatGradeDto toSeatGradeDto(Grade grade) {
+        return PerformanceSeatGradeDto.builder()
+                .grade(grade.getGrade())
+                .defaultPrice(grade.getDefaultPrice())
+                .seatCount(grade.getSeatCount())
+                .build();
+    }
+}
--- src/main/java/com/fifo/ticketing/domain/performance/service/PerformanceService.java
+++ src/main/java/com/fifo/ticketing/domain/performance/service/PerformanceService.java
@@ -1,12 +1,21 @@
 package com.fifo.ticketing.domain.performance.service;
 
+import static com.fifo.ticketing.global.exception.ErrorCode.NOT_FOUND_PERFORMANCE;
 import static com.fifo.ticketing.global.exception.ErrorCode.NOT_FOUND_PERFORMANCES;
 
+import com.fifo.ticketing.domain.performance.dto.PerformanceDetailResponse;
+import com.fifo.ticketing.domain.performance.dto.PerformanceMapper;
+import com.fifo.ticketing.domain.performance.dto.PerformanceSeatGradeDto;
 import com.fifo.ticketing.domain.performance.entity.Category;
+import com.fifo.ticketing.domain.performance.entity.Grade;
 import com.fifo.ticketing.domain.performance.entity.Performance;
+import com.fifo.ticketing.domain.performance.repository.GradeRepository;
 import com.fifo.ticketing.domain.performance.repository.PerformanceRepository;
 import com.fifo.ticketing.global.exception.ErrorException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,11 +27,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class PerformanceService {
 
     private final PerformanceRepository performanceRepository;
+    private final GradeRepository gradeRepository;
+
+    @Transactional(readOnly = true)
+    public PerformanceDetailResponse getPerformanceDetail(Long performanceId) {
+        Performance performance = performanceRepository.findById(performanceId)
+                .orElseThrow(() -> new ErrorException(NOT_FOUND_PERFORMANCE));
+
+        List<Grade> grades = gradeRepository.findByPlaceId(performance.getPlace().getId());
+        List<PerformanceSeatGradeDto> seatGrades = grades.stream()
+                .map(PerformanceMapper::toSeatGradeDto)
+                .toList();
+
+        return PerformanceMapper.toDetailResponseDto(performance, seatGrades);
+    }
 
     @Transactional(readOnly = true)
     public Page<Performance> getPerformancesSortedByLatest(Pageable pageable) {
         Page<Performance> upcomingPerformances = performanceRepository.findUpcomingPerformancesOrderByReservationStartTime(
-            LocalDateTime.now(), pageable);
+                LocalDateTime.now(), pageable);
         if (upcomingPerformances.isEmpty()) {
             throw new ErrorException(NOT_FOUND_PERFORMANCES);
         }
@@ -32,7 +55,7 @@ public class PerformanceService {
     @Transactional(readOnly = true)
     public Page<Performance> getPerformancesSortedByLikes(Pageable pageable) {
         Page<Performance> upcomingPerformances = performanceRepository.findUpcomingPerformancesOrderByLikes(
-            LocalDateTime.now(), pageable);
+                LocalDateTime.now(), pageable);
         if (upcomingPerformances.isEmpty()) {
             throw new ErrorException(NOT_FOUND_PERFORMANCES);
         }
@@ -41,9 +64,9 @@ public class PerformanceService {
 
     @Transactional(readOnly = true)
     public Page<Performance> getPerformancesByReservationPeriod(LocalDateTime start,
-        LocalDateTime end, Pageable pageable) {
+                                                                LocalDateTime end, Pageable pageable) {
         Page<Performance> upcomingPerformances = performanceRepository.findUpcomingPerformancesByReservationPeriod(
-            start, end, pageable);
+                start, end, pageable);
         if (upcomingPerformances.isEmpty()) {
             throw new ErrorException(NOT_FOUND_PERFORMANCES);
         }
@@ -53,7 +76,7 @@ public class PerformanceService {
     @Transactional(readOnly = true)
     public Page<Performance> getPerformancesByCategory(Category category, Pageable pageable) {
         Page<Performance> upcomingPerformances = performanceRepository.findUpcomingPerformancesByCategory(
-            LocalDateTime.now(), category, pageable);
+                LocalDateTime.now(), category, pageable);
         if (upcomingPerformances.isEmpty()) {
             throw new ErrorException(NOT_FOUND_PERFORMANCES);
         }
