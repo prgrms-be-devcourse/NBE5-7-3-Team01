c1b0acd7c03a7d67a1a7e2fe16f4c63f4ec6160c
chaechae <juold0568@naver.com>
2025-05-12 11:22:34 +0900

    fix: 충돌 해결

--- src/main/java/com/fifo/ticketing/global/exception/ErrorCode.java
+++ src/main/java/com/fifo/ticketing/global/exception/ErrorCode.java
@@ -11,8 +11,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     NOT_FOUND_MEMBER("AUTH-001", "존재하지 않는 회원입니다.", NOT_FOUND),
-    SEAT_ALREADY_BOOKED("임시","해당 좌석은 이미 예약되었습니다.",SEAT_BOOKING_FAILED),
-    NOT_FOUND_PERFORMANCES("AUTH-001", "존재하지 않는 회원입니다.", NOT_FOUND),
     NOT_FOUND_PERFORMANCES("PERFORMANCE-001", "예매 가능한 공연이 존재하지 않습니다.", NOT_FOUND),
     NOT_FOUND_PERFORMANCE("PERFORMANCE-002", "존재하지 않는 공연입니다.", NOT_FOUND),
     SEAT_ALREADY_BOOKED("SEAT-001", "해당 좌석은 이미 예약되었습니다.", CONFLICT),
