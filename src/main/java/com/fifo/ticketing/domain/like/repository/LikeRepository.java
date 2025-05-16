f793209cfbac30d93f3ddbcf2251fcc40c6c072f
ZELDA31777 <102502882+ZELDA31777@users.noreply.github.com>
2025-05-16 20:59:02 +0900

    [fix]: 마이페이지에서 좋아요한 공연 목록에서 삭제된 공연이 조회되는 문제 (#118)

--- src/main/java/com/fifo/ticketing/domain/like/repository/LikeRepository.java
+++ src/main/java/com/fifo/ticketing/domain/like/repository/LikeRepository.java
@@ -20,16 +20,14 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     @Query("SELECT l.user FROM Like l WHERE l.performance.id = :performanceId AND l.isLiked = true")
     List<User> findUsersByPerformanceId(@Param("performanceId") Long performanceId);
 
-
     @Query("SELECT l FROM Like l WHERE l.isLiked =true AND l.performance.reservationStartTime BETWEEN :start AND :end")
-    List<Like> findLikesByTargetTime(@Param("start") LocalDateTime start ,  @Param("end") LocalDateTime end);
+    List<Like> findLikesByTargetTime(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
+    // 해당 Method가 사용되지 않는 것 같은데, 제거 해도 괜찮을까요?
     @Query("SELECT l.performance.id FROM Like l WHERE l.user.id = :userId AND l.isLiked = true")
     List<Long> findLikedPerformanceIdsByUserId(@Param("userId") Long userId);
 
-    @Query("SELECT l.performance FROM Like l WHERE l.user.id = :userId AND l.isLiked = true ORDER BY l.performance.startTime ASC")
-    Page<Performance> findLikedPerformancesByUserId(@Param("userId") Long userId,
-        Pageable pageable);
-
+    @Query("SELECT l.performance FROM Like l WHERE l.performance.deletedFlag != true AND l.user.id = :userId AND l.isLiked = true ORDER BY l.performance.startTime ASC")
+    Page<Performance> findLikedPerformancesByUserId(@Param("userId") Long userId, Pageable pageable);
 
 }
