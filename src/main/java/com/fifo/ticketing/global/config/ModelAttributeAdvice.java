054278cec8f91cbc1a3941cd5a9fd07f5565b46e
chcch529 <146617430+chcch529@users.noreply.github.com>
2025-05-20 09:09:26 +0900

    [fix] 사용자 페이지 헤더 추가 (#163)
    * fix: userId 기반 로그인 체크
    
    * fix: 사용자 페이지 헤더 추가
    
    * feat: 헤더 로그인 체크를 위한 ControllerAdvice 추가
    
    * fix: 사용자 페이지 헤더 추가
    
    * fix: 모달 출력 오류 수정
    
    * style: 인덱스 페이지 재구성
    
    * fix: 로고 버튼 경로 수정
    
    * fix: 공연 목록 페이지 헤더 삭제
    
    * fix: 공연 목록 페이지 헤더 추가

---
+++ src/main/java/com/fifo/ticketing/global/config/ModelAttributeAdvice.java
@@ -0,0 +1,18 @@
+package com.fifo.ticketing.global.config;
+
+import com.fifo.ticketing.domain.user.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class ModelAttributeAdvice {
+    @ModelAttribute
+    public void addUserIdToModel(HttpSession session, Model model) {
+        SessionUser loginUser = (SessionUser) session.getAttribute("loginUser");
+        if (loginUser != null) {
+            model.addAttribute("userId", loginUser.id());
+        }
+    }
+}
