7f2c0781b640b5b905b3971afa2f1cd72353bc8c
나상연 <reddynor@naver.com>
2025-06-12 09:45:18 +0900

    feat: SessionUser의 경우 @Valid 제거

--- src/main/java/com/fifo/ticketing/domain/user/dto/SessionUser.kt
+++ src/main/java/com/fifo/ticketing/domain/user/dto/SessionUser.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotEmpty
 
 @JvmRecord
 data class SessionUser(
-    @JvmField val id: @NotEmpty(message = "id는 필수입니다.") Long?,
-    @JvmField val username: @NotEmpty(message = "유저명은 필수입니다.") String?,
-    @JvmField val role: @NotEmpty(message = "유저권한은 필수입니다.") Role?
+    @JvmField val id: Long,
+    @JvmField val username: String,
+    @JvmField val role: Role
 )
