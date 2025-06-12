e03eb8c0640e0c810c291a58d05eb699a9bd03f9
chaechae <juold0568@naver.com>
2025-06-12 10:17:40 +0900

    feat: SessionUser에 getter 추가

--- src/main/java/com/fifo/ticketing/domain/user/dto/SessionUser.java
+++ src/main/java/com/fifo/ticketing/domain/user/dto/SessionUser.java
@@ -7,4 +7,18 @@ public record SessionUser(@NotEmpty(message = "id는 필수입니다.") Long id,
                           @NotEmpty(message = "유저명은 필수입니다.") String username,
                           @NotEmpty(message = "유저권한은 필수입니다.") Role role) {
 
+    @Override
+    public Long id() {
+        return id;
+    }
+
+    @Override
+    public String username() {
+        return username;
+    }
+
+    @Override
+    public Role role() {
+        return role;
+    }
 }
