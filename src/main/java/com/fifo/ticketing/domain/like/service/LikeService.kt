6696a6655f0f32490e4058f484d728d1ced14ca1
hs986 <94986211+hs986@users.noreply.github.com>
2025-06-12 12:33:47 +0900

    fix : builder로 변경

--- src/main/java/com/fifo/ticketing/domain/like/service/LikeService.kt
+++ src/main/java/com/fifo/ticketing/domain/like/service/LikeService.kt
@@ -11,31 +11,38 @@ import com.fifo.ticketing.domain.user.repository.UserRepository
 import com.fifo.ticketing.global.exception.ErrorCode
 import com.fifo.ticketing.global.exception.ErrorException
 import jakarta.transaction.Transactional
-import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 import kotlin.math.max
 
 @Service
-class LikeService (
+class LikeService(
     private val likeRepository: LikeRepository,
     private val likeCountRepository: LikeCountRepository,
     private val userRepository: UserRepository,
     private val performanceRepository: PerformanceRepository
-
-    ){
+) {
 
     @Transactional
     fun toggleLike(userId: Long, likeRequest: LikeRequest): Boolean {
         val user = userRepository.findById(userId)
-            .orElseThrow { ErrorException("존재하지 않는 회원입니다.", ErrorCode.NOT_FOUND_MEMBER) }
+            .orElseThrow {
+                ErrorException(
+                    ErrorCode.NOT_FOUND_MEMBER,
+                    "존재하지 않는 회원입니다."
+                )
+            }
 
         val performance = performanceRepository.findById(likeRequest.performanceId)
-            .orElseThrow { ErrorException("예매 가능한 공연이 존재하지 않습니다.", ErrorCode.NOT_FOUND_PERFORMANCES) }
+            .orElseThrow {
+                ErrorException(
+                    ErrorCode.NOT_FOUND_PERFORMANCES,
+                    "예매 가능한 공연이 존재하지 않습니다."
+                )
+            }
 
         val existingLike = likeRepository.findByUserAndPerformance(user, performance)
 
-        //like가 없다면
-        if (existingLike == null) {
+        return if (existingLike == null) {
             val like = Like.builder()
                 .user(user)
                 .performance(performance)
@@ -43,38 +50,33 @@ class LikeService (
                 .build()
             likeRepository.save(like)
             updateLike(performance, 1)
-            // likecount 제거
-            return true
-        } //없다ㅕㄴ
-
-
-        if (existingLike.isLiked) {
-            existingLike.isLiked = false
+            true
+        } else {
+            val isNowLiked = !existingLike.isLiked
+            existingLike.isLiked = isNowLiked
             likeRepository.save(existingLike)
-            updateLike(performance, -1)
-            // likeCOunt 추가
-            return false
+            updateLike(performance, if (isNowLiked) 1 else -1)
+            isNowLiked
         }
-
-        existingLike.isLiked = true
-        likeRepository.save(existingLike)
-        updateLike(performance, 1)
-        return true
     }
 
     private fun updateLike(performance: Performance, cnt: Int) {
-        val likeCount: LikeCount = likeCountRepository.findByPerformance(performance)?:
-        throw  ErrorException("예매 가능한 공연이 존재하지 않습니다.",ErrorCode.NOT_FOUND_PERFORMANCES)
+        val likeCount = likeCountRepository.findByPerformance(performance)?:
+        throw  ErrorException(ErrorCode.NOT_FOUND_PERFORMANCES,"예매 가능한 공연이 존재하지 않습니다.")
 
-        var updatedCnt = likeCount.likeCount + cnt
-        updatedCnt = max(0.0, updatedCnt.toDouble()).toLong() //  음수 방지
+        val updatedCnt = max(0L, likeCount.likeCount + cnt)
         likeCount.setLikeCount(updatedCnt)
         likeCountRepository.save(likeCount)
     }
 
     fun getLikedPerformancesIds(userId: Long): List<Long> {
-        userRepository.findById(userId)?:
-        throw ErrorException("존재하지 않는 회원입니다.", ErrorCode.NOT_FOUND_MEMBER)
+        userRepository.findById(userId)
+            .orElseThrow {
+                ErrorException(
+                    ErrorCode.NOT_FOUND_MEMBER,
+                    "존재하지 않는 회원입니다."
+                )
+            }
 
         return likeRepository.findLikedPerformanceIdsByUserId(userId)
     }
