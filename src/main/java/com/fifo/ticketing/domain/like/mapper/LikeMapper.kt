074e441882193bbdc551e920dbb75fdabd5820bd
hs986 <94986211+hs986@users.noreply.github.com>
2025-06-12 16:43:24 +0900

    chore : 코드 간략화

--- src/main/java/com/fifo/ticketing/domain/like/mapper/LikeMapper.kt
+++ src/main/java/com/fifo/ticketing/domain/like/mapper/LikeMapper.kt
@@ -5,8 +5,6 @@ import com.fifo.ticketing.domain.performance.entity.Performance
 import com.fifo.ticketing.domain.user.entity.User
 
 object LikeMapper {
-    fun create(user: User, performance: Performance): Like {
-        val like = Like(null, user, performance, true)
-        return like
-    }
+    fun create(user: User, performance: Performance) = Like(null, user, performance, true)
+
 }
