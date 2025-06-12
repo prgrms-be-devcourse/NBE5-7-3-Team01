65d8452b7d3d9414d3ec45c834666e0df9f0844e
ZELDA31777 <3177link@gmail.com>
2025-06-12 10:02:10 +0900

    fix: GradeRepository의 Null 허용 수정

--- src/main/java/com/fifo/ticketing/domain/performance/repository/GradeRepository.kt
+++ src/main/java/com/fifo/ticketing/domain/performance/repository/GradeRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GradeRepository : JpaRepository<Grade, Long?> {
+interface GradeRepository : JpaRepository<Grade, Long> {
     fun findAllByPlaceId(id: Long): List<Grade>
 }
