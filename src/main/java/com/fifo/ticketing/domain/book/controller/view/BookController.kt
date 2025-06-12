875d5d947aabe7778e827030206cd017c040b3a7
chaechae <juold0568@naver.com>
2025-06-12 15:15:39 +0900

    feat: 컨트롤러 코틀린 변환

--- src/main/java/com/fifo/ticketing/domain/book/controller/view/BookController.kt
+++ src/main/java/com/fifo/ticketing/domain/book/controller/view/BookController.kt
@@ -1,71 +1,65 @@
-package com.fifo.ticketing.domain.book.controller.view;
+package com.fifo.ticketing.domain.book.controller.view
 
-import com.fifo.ticketing.domain.book.dto.BookCompleteDto;
-import com.fifo.ticketing.domain.book.dto.BookCreateRequest;
-import com.fifo.ticketing.domain.book.dto.BookMailSendDto;
-import com.fifo.ticketing.domain.book.mapper.BookMapper;
-import com.fifo.ticketing.domain.book.service.BookMailService;
-import com.fifo.ticketing.domain.book.service.BookService;
-import com.fifo.ticketing.domain.performance.repository.PerformanceRepository;
-import com.fifo.ticketing.domain.seat.repository.SeatRepository;
-import com.fifo.ticketing.domain.user.dto.SessionUser;
-import com.fifo.ticketing.global.util.UserValidator;
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.fifo.ticketing.domain.book.dto.BookCreateRequest
+import com.fifo.ticketing.domain.book.service.BookKtService
+import com.fifo.ticketing.domain.book.service.BookMailService
+import com.fifo.ticketing.domain.book.service.BookService
+import com.fifo.ticketing.global.util.UserValidator.validateSessionUser
+import jakarta.servlet.http.HttpSession
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/performances/{performanceId}/book")
-public class BookController {
-
-    private final BookService bookService;
-    private final BookMailService bookMailService;
+class BookController(
+    private val bookService: BookService,
+    private val bookKtService: BookKtService,
+    private val bookMailService: BookMailService,
+) {
 
     @PostMapping
-    public String createBook(
-        @PathVariable Long performanceId,
-        HttpSession session,
-        @RequestParam List<Long> seatIds
-    ) {
-        SessionUser loginUser = UserValidator.validateSessionUser(session);
-        BookCreateRequest request = new BookCreateRequest(seatIds);
-        Long bookId = bookService.createBook(performanceId, loginUser.id, request);
-        return "redirect:/performances/" + performanceId + "/book/complete/" + bookId;
-
+    suspend fun createBook(
+        @PathVariable performanceId: Long,
+        session: HttpSession,
+        @RequestParam seatIds: List<Long>
+    ): String {
+        val loginUser = validateSessionUser(session)
+        val request = BookCreateRequest(seatIds)
+        val bookId = bookKtService.createBook(performanceId, loginUser.id, request)
+        return "redirect:/performances/$performanceId/book/complete/$bookId"
     }
 
     @PostMapping("/complete/{bookId}/paid")
-    public String completePayment(@PathVariable Long performanceId, @PathVariable Long bookId,
-        RedirectAttributes redirectAttributes) {
-        bookService.completePayment(bookId);
+    fun completePayment(
+        @PathVariable performanceId: Long, @PathVariable bookId: Long,
+        redirectAttributes: RedirectAttributes
+    ): String {
+        bookService.completePayment(bookId)
 
-        BookMailSendDto bookMailInfo = bookService.getBookMailInfo(bookId);
-        bookMailService.sendBookCompleteMail(bookMailInfo);
+        val bookMailInfo = bookService.getBookMailInfo(bookId)
+        bookMailService.sendBookCompleteMail(bookMailInfo)
 
-        redirectAttributes.addAttribute("paid", true);
-        return "redirect:/performances/" + performanceId + "/book/complete/" + bookId;
+        redirectAttributes["paid"] = true
+        return "redirect:/performances/$performanceId/book/complete/$bookId"
     }
 
     @GetMapping("/complete/{bookId}")
-    public String viewBookingComplete(
-        @PathVariable Long bookId,
-        @RequestParam(value = "paid", required = false, defaultValue = "false") boolean paid,
-        Model model) {
+    fun viewBookingComplete(
+        @PathVariable bookId: Long,
+        @RequestParam(value = "paid", required = false, defaultValue = "false") paid: Boolean,
+        model: Model
+    ): String {
+        val bookCompleteInfo = bookService.getBookCompleteInfo(bookId)
+        bookCompleteInfo.paymentCompleted = paid
 
-        BookCompleteDto bookCompleteInfo = bookService.getBookCompleteInfo(bookId);
-        bookCompleteInfo.setPaymentCompleted(paid);
+        model["book"] =  bookCompleteInfo
+        model["bookId"] =  bookId
 
-        model.addAttribute("book", bookCompleteInfo);
-        model.addAttribute("bookId", bookId);
-
-        return "book/complete";
+        return "book/complete"
     }
-
-
 }
