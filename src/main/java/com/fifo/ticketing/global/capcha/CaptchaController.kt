b735498e6f39b66d28ed796d0abe22fac7fcade1
나상연 <reddynor@naver.com>
2025-06-16 14:25:05 +0900

    feat: captcha에 redis 적용

--- src/main/java/com/fifo/ticketing/global/capcha/CaptchaController.kt
+++ src/main/java/com/fifo/ticketing/global/capcha/CaptchaController.kt
@@ -1,5 +1,6 @@
 package com.fifo.ticketing.global.capcha
 
+import com.fifo.ticketing.domain.user.service.RedisService
 import com.fifo.ticketing.global.capcha.dto.CaptchaResponse
 import com.fifo.ticketing.global.capcha.dto.CaptchaVerifyRequest
 import org.springframework.http.ResponseEntity
@@ -9,32 +10,31 @@ import java.util.*
 @RestController
 @RequestMapping("/api/captcha")
 class CaptchaController(
-    private val captchaStorage: CaptchaStore
+    private val captchaStorage: RedisService
 ) {
-
 
     @GetMapping
     fun generateCaptcha(): CaptchaResponse {
         val randomText = generateRandomText()
         val id = UUID.randomUUID().toString()
+        val key = "captcha:$id"
 
-        captchaStorage[id] = randomText;
+        captchaStorage.setValuesWithTimeout(key, randomText, 3 * 60 * 1000)
 
         val image = createCaptchaImage(randomText)
         val imageBase64 = Base64.getEncoder().encodeToString(image)
 
-        return CaptchaResponse(id, "data:image/png;base64,$imageBase64")
+        return CaptchaResponse(key, "data:image/png;base64,$imageBase64")
     }
 
     @PostMapping("/verify")
     fun verifyCaptcha(@RequestBody req: CaptchaVerifyRequest): ResponseEntity<String> {
-        val expected = captchaStorage[req.captchaId] ?: return ResponseEntity.status(403)
+        val expected = captchaStorage.getValues(req.captchaId) ?: return ResponseEntity.status(403)
             .body("유효 시간이 만료되었습니다.")
         if (expected != req.input) return ResponseEntity.status(403).body("잘못된 인증 번호입니다.")
 
-        captchaStorage.remove(req.captchaId)
+        captchaStorage.deleteValues(req.captchaId)
         return ResponseEntity.ok("올바른 입력입니다.")
     }
-
 
 }
