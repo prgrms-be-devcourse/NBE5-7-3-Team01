2f86b7ddd745c4f259ed780e01d1b275b083df6f
chaechae <juold0568@naver.com>
2025-06-16 09:48:49 +0900

    fix: jpaconfig ci 제외

--- src/main/java/com/fifo/ticketing/global/config/JpaAuditingConfig.kt
+++ src/main/java/com/fifo/ticketing/global/config/JpaAuditingConfig.kt
@@ -2,8 +2,10 @@ package com.fifo.ticketing.global.config
 
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @Configuration
+@Profile("!ci")
 @EnableJpaAuditing
 class JpaAuditingConfig
