caad1e21b40b201975510f4809846ae827b1005a
ZELDA31777 <102502882+ZELDA31777@users.noreply.github.com>
2025-06-16 10:23:59 +0900

    [feat] RestExceptionAdvice 구현 (#53)
    * feat: Rest API Validation 예외처리 추가
    
    * feat: 예약 시작 시간에 대한 예외 추가

--- src/main/java/com/fifo/ticketing/global/validation/ValidPerformanceDatesValidator.kt
+++ src/main/java/com/fifo/ticketing/global/validation/ValidPerformanceDatesValidator.kt
@@ -14,11 +14,17 @@ class ValidPerformanceDatesValidator :
 
         // startTime < endTime
         if (dto.startTime.isAfter(dto.endTime) || dto.startTime.isEqual(dto.endTime)) {
+            context.disableDefaultConstraintViolation()
+            context.buildConstraintViolationWithTemplate("INVALID_DATETIME_PERIOD")
+                .addConstraintViolation()
             return false
         }
 
         // reservationStartTime <= startTime
         if (dto.reservationStartTime.isAfter(dto.startTime)) {
+            context.disableDefaultConstraintViolation()
+            context.buildConstraintViolationWithTemplate("INVALID_DATETIME_RESERVATION")
+                .addConstraintViolation()
             return false
         }
 
