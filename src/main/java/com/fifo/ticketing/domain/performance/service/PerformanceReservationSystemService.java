c004e263e779001812d5f365054ba59daff154ce
Jiwon Kwak <koagjw@gmail.com>
2025-06-17 16:42:29 +0900

    [fix] 좌석이 매진되고 취소됨에 따라 공연 예매 가능 여부를 바꾸는 메서드 추가 (#82)
    * :bug: fix: 좌석 매진/예매 취소에 따라 예매 가능 상태 바꾸는 로직 추가
    
    * :truck: chore: 클래스 이름 변경

---
+++ src/main/java/com/fifo/ticketing/domain/performance/service/PerformanceReservationSystemService.java
@@ -0,0 +1,35 @@
+package com.fifo.ticketing.domain.performance.service;
+
+import com.fifo.ticketing.domain.performance.repository.PerformanceRepository;
+import com.fifo.ticketing.domain.seat.repository.SeatRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PerformanceReservationSystemService {
+
+    private final PerformanceRepository performanceRepository;
+    private final SeatRepository seatRepository;
+
+    @Transactional
+    public void updateStatusIfReservationStart() {
+        performanceRepository.updatePerformanceStatusToReservationStart(LocalDateTime.now());
+    }
+
+    @Transactional
+    public void updateStatusIfSoldOutOrCanceled() {
+        performanceRepository.findActivePerformances((LocalDateTime.now())).forEach(performance -> {
+            Long performanceId = performance.getId();
+            int availableSeats = seatRepository.countAvailableSeatsByPerformanceId(performanceId);
+
+            if (availableSeats == 0) {
+                performanceRepository.updatePerformanceStatusReservationUnavailable(performanceId);
+            } else if (availableSeats > 0) {
+                performanceRepository.updatePerformanceStatusReservationAvailable(performanceId);
+            }
+        });
+    }
+}
