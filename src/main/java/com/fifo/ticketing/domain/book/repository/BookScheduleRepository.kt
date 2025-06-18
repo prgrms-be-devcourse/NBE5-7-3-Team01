7edc97dd96fdf840aebe5ab3af0469953c459659
jiwon1217 <koagjw@gmail.com>
2025-06-18 12:11:31 +0900

    :sparkles: feat: BookScheduleRepository kotlin으로 전환

--- src/main/java/com/fifo/ticketing/domain/book/repository/BookScheduleRepository.kt
+++ src/main/java/com/fifo/ticketing/domain/book/repository/BookScheduleRepository.kt
@@ -1,15 +1,13 @@
-package com.fifo.ticketing.domain.book.repository;
+package com.fifo.ticketing.domain.book.repository
 
-import com.fifo.ticketing.domain.book.entity.BookScheduledTask;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import com.fifo.ticketing.domain.book.entity.BookScheduledTask
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
 @Repository
-public interface BookScheduleRepository extends JpaRepository<BookScheduledTask, Long> {
+interface BookScheduleRepository : JpaRepository<BookScheduledTask, Long> {
 
     @Query("SELECT s FROM BookScheduledTask s WHERE s.taskStatus = 'PENDING'")
-    List<BookScheduledTask> findAllPendingTasks();
-
+    fun findAllPendingTasks(): List<BookScheduledTask>
 }
